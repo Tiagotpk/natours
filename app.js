e6cd55625ecd8d20a41fe57253e5a40963fdbea8
Tiago Reis <ttrpk@hotmail.com>
2025-08-05 21:08:25 -0300

    iniciando o projeto Natours

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the srver side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
